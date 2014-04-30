1099c4612a32d8e7ec37a6d715e123d8c5ab9093
Orion <orioncharlier@gmail.com>
2014-04-30 19:31:51 +0200

    News feature
    Basics of news feature

--- www/js/controllers/article.js
+++ www/js/controllers/article.js
@@ -1,4 +1,11 @@
 angular.module('driveme')
-  .controller('ArticleCtrl', function ($scope, $stateParams) {
-    $scope.id = $stateParams.id;
+  .controller('ArticleCtrl', function ($scope, $stateParams, Restangular) {
+
+	  $scope.getArticle = function () {
+			Restangular.one('news', $stateParams.id).get().then(function (article) {
+				$scope.article = article[0];
+			});
+	  }
+
+	  $scope.getArticle();
   });
