317e9f344f89875d914362a9a120e553f7acc070
Jean-Baptiste Beuzelin <jbbeuzelin@gmail.com>
2014-05-14 01:30:51 +0200

    DIP : sign up v3

--- www/js/controllers/signup.js
+++ www/js/controllers/signup.js
@@ -1,3 +1,45 @@
+var test;
+var _scope;
 angular.module('driveme')
-  .controller('SignUpCtrl', function ($scope, $state) {
+	.controller('SignUpCtrl', function ($scope, $state) 
+	{
+		$scope.userSignUp = 
+			{
+				firstname : "",
+				lastname : "",
+				username : "",
+				passwordA : "",
+				passwordB : "",
+				same : "false",
+			};
+		test = $scope.userSignUp;
+
+		$scope.change = function(){
+			var firstname = '';
+			var lastname = '';
+			if(typeof $scope.userSignUp.firstname !== 'undefined')
+			{
+				firstname =$scope.userSignUp.firstname;
+			}
+			if(typeof $scope.userSignUp.lastname !== 'undefined')
+			{
+				lastname = $scope.userSignUp.lastname;
+			}
+			var point = (firstname !== "" || lastname !== "") ? "." : "" ;
+			$scope.userSignUp.username = firstname +point+lastname;
+		};
+
+		$scope.checkPassword = function(){
+			_scope=$scope;
+			$scope.userSignUp.same = $scope.userSignUp.passwordA === $scope.userSignUp.passwordB;
+			if(!$scope.userSignUp.same)
+			{
+				signUpForm.passwordB.$setValidity('pwd', false); 
+			}
+		};
+
+
+
+
+
 	});
